844f807ef41be29517cb7cd907b42231398afcbd
COBKingdom <cobwave01@gmail.com>
2026-04-13 19:22:23 +0100

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css"
+import Script from "next/script"
 
 export const metadata = {
   title: "AquaOps",
@@ -16,7 +17,23 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2G1MMLJZGH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2G1MMLJZGH');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   )
 }
